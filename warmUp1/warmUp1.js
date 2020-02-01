1eed46266ad49c6b6db5a43a49d32181c8d6e31e
GhaziTurki <60433277+GhaziTurki@users.noreply.github.com>
2020-02-01 13:37:06 +0100

    Saturday exercice

--- warmUp1/warmUp1.js
+++ warmUp1/warmUp1.js
@@ -3,4 +3,27 @@
 // 3-calculate the average age of the follwing ages [13,14,13,15,16,17,19,13,16,15]
 // 4-calculate your age in seconds.
 
-// your code is here
+
+
+var firstName="Ghazi"; var lastName="Turki";
+console.log (firstName,lastName,)
+
+function(findingOut) {
+
+}
+
+
+function(array) {
+var result = 0
+for (var i =0,i<array.length,i=i+1) {
+
+    result = result + array[i]
+}
+
+return result/array.length
+}
+
+
+function(myAge) {
+return 40*356*24*360
+}
