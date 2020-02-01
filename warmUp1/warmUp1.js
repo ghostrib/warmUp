94647f8b16c8f6f27b5198915da459b4bef5eea6
Mohamed Zemzemi <mzemzemi9@gmail.com>
2020-02-01 13:33:45 +0100

    Done warmUp

--- warmUp1/warmUp1.js
+++ warmUp1/warmUp1.js
@@ -4,3 +4,51 @@
 // 4-calculate your age in seconds.
 
 // your code is here
+//////////////////////////////////  1
+function partner(firstName, lastName){
+    return(firstName+' '+lastName)
+}
+//////////////////////////////////  2
+if(13%3===0){
+     console.log('13 is a multiple of 3')
+}else
+     console.log( '13 is not a multiple of 3');
+//////////////////////////////////  3
+function average(array){
+    var s=0;
+    for(var i=0;i<array.length;i++){
+     
+   s=s+array[i];
+    
+} return s/array.length}
+//////////////////////////////////  4
+var m=0;
+function ages(age,month,day){
+   m=((month-1)*30+day)*24*60*60;
+
+    return age*365*24*60*60+m
+}
+////////////// other methode not finished yet
+/*var months={
+    january: 31,
+    february: 29,
+    march:31,
+    april:30,
+    may:31,
+    june:30,
+    july:31,
+    august:30,
+    september:31,
+    october:30,
+    november:31,
+    december:30
+}
+var m=0;
+var s=0;
+    for(var i=0;i<months.length;i++){
+   s=s+array[i];
+function ages(age,month,day){
+   m=((month-1)*30+day)*24*60*60;
+
+    return age*365*24*60*60+m
+}*/
