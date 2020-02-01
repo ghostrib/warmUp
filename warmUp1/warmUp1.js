b6ce42cdc80e7761dd54cd64f9d6dfb875d29101
abdellihou <abdelli.hou@gmail.com>
2020-02-01 09:56:43 +0100

    HoussemEddineABDELLI

--- warmUp1/warmUp1.js
+++ warmUp1/warmUp1.js
@@ -4,3 +4,53 @@
 // 4-calculate your age in seconds.
 
 // your code is here
+
+
+
+//1
+var firstName = "Houssem Eddine";
+var lastName = "ABDELLI";
+var fullName = " ";
+fullName = firstName + " " + lastName
+console.log(fullName)
+
+
+
+//2
+var x = 13;
+if (x % 3 === 0) {
+	console.log(x + "is a multiple of 3")
+}
+console.log(x + " is not a multiple of 3")
+
+
+
+
+//3
+var array = [13, 14, 13, 15, 16, 17, 19, 13, 16, 15];
+var average = 0;
+for (var i = 0; i < array.length; i++) {
+	average = (array[i] / array.length) + average;
+}
+console.log(average)
+
+
+
+
+//4
+var ageInYears = 33;
+var ageInSeconds = 0;
+var x = 0;
+x = 365.25 * 24 * 60 * 60;
+ageInSeconds = x * ageInYears;
+console.log(ageInSeconds)
+
+
+
+
+function ageInSeconds(ageInYears, monthsAdded) {
+	ageSeconds = 365.25 * 24 * 60 * 60 * ageInYears
+	monthSeconds = 30 *
+
+
+}
