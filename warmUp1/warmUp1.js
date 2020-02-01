5d5b88eb8b2cfaca33d1d162d6d8a9eb4dc2b15c
halimajilani <halima.jilani@gmail.com>
2020-02-01 10:00:29 +0100

    age

--- warmUp1/warmUp1.js
+++ warmUp1/warmUp1.js
@@ -1,6 +1,36 @@
 // 1-using + operator combine your partner first and last name .
+var operator={
+    firstName: "halima",
+    lastName: "jilani",
+}
 // 2-find if the number 13 is a multiple of 3 or not.
+function equal(number){
+    if (i=0){
+        number%i;
+        return "herro";
+    }
+    else if (i>0|| i<0){
+        number%i= 0;
+        return "number is a multiple of i";
+    }
+    return "number is not multiple of i";
+ 
+}
 // 3-calculate the average age of the follwing ages [13,14,13,15,16,17,19,13,16,15]
+faunction sumarray(array){
+    var i = 0;
+    if (i<= array.length-1){
+    return [i] + sumarray(array);
+}
+return "error"
+
+}
+function overage(array){
+    return samarray(arra)/array.length
+}
 // 4-calculate your age in seconds.
+    
+    
+
 
 // your code is here
