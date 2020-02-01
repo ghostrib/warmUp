27be90d0245558127437625095681835badf74f6
Bouabda <hamam.alsaiity@gmail>
2020-02-01 10:04:16 +0100

    warmUp1 wala

--- warmUp1/warmUp1.js
+++ warmUp1/warmUp1.js
@@ -3,4 +3,24 @@
 // 3-calculate the average age of the follwing ages [13,14,13,15,16,17,19,13,16,15]
 // 4-calculate your age in seconds.
 
-// your code is here
+//===============================1
+var partner = "Amine" + "Hassine";
+//===============================2
+13 % 3;
+// if the result is '0' , '13' is a multiple of 3 .
+//===============================3
+function average(arr) {
+  for (i = 0; i < arr.length; i++) {
+    var x = x + arr[i];
+  }
+  return x / arr.length;
+}
+//===============================4
+function age(dOB, mOB, yOB) {
+  var today = { day, month, year };
+  var y = today.year - yOB;
+  var m = today.month - mOB;
+  var d = today.day - dOB;
+  var myAge = y * 365 * 3600 + m * 30 * 3600 + d * 3600;
+  return myAge;
+}
