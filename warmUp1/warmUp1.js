c80ca4986b92ec15de446de9046fa59e44ddca5a
ghostrib <sh4rkbot@gmail.com>
2020-02-05 15:27:47 +0100

    warmup1 complete

--- warmUp1/warmUp1.js
+++ warmUp1/warmUp1.js
@@ -1,8 +1,31 @@
 // 1-using + operator combine your partner first and last name .
+
+function makeName(first, last) {
+  return first + ' ' + last;
+}
 // 2-find if the number 13 is a multiple of 3 or not.
+
+function isMultipleOf13(n) {
+  return n % 13 === 0;
+}
+
 // 3-calculate the average age of the follwing ages [13,14,13,15,16,17,19,13,16,15]
+
+function average(arr) {
+  var sum = 0;
+  for (var i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum / 2;
+}
+
 // 4-calculate your age in seconds.
 
-// your code is here
-
-
+function calculateAgeInSeconds(age) {
+  var seconds = 60;
+  var minutes = 60;
+  var hours = 24;
+  var days = 364.25;
+  var result = age * seconds * minutes * hours * days;
+  return result;
+}
